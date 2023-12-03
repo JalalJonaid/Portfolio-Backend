5c67d898bd41b7625c828daee99e66709d2bcab2
Jalal Jonaid <jalaljonaid@jalals-air.myfiosgateway.com>
2023-12-03 09:25:30 -0500

    Working server

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require ('express');
-const cors = require('cors');
+
+//const cors = require('cors');
+
 const app = express();
+
 const matchupsController = require('./controllers/matchupsController.js')
-app.use(cors());
+//app.use(cors());
+
 app.use(express.json());
+
 app.use("/matchups", matchupsController);
 
 app.get("/", (req, res) => {
     res.send("Welcome to K'sante Matchups Guide!");
   });
   
-  app.get("*", (req, res) => {
+app.get("*", (req, res) => {
     res.status(404).json({ success: false, data: { error: "page not found" } });
-  });
+});
 
   module.exports = app;
