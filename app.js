50ed6ad6439d43edcc7b96ffe4d4e661dc87c555
Jalal Jonaid <jalaljonaid@jalals-air.myfiosgateway.com>
2023-12-03 10:39:26 -0500

    Added dependencies

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require ('express');
-
-//const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 
 const matchupsController = require('./controllers/matchupsController.js')
-//app.use(cors());
+app.use(cors());
 
 app.use(express.json());
 
